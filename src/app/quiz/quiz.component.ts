155ff4c6a03538dd7604c1dfbc507f96a662c14c
Mat <71132805+mat512@users.noreply.github.com>
2024-09-24 12:33:59 +0200

    refactor: quiz

--- src/app/quiz/quiz.component.ts
+++ src/app/quiz/quiz.component.ts
@@ -1,19 +1,29 @@
-import { Component, OnInit, Signal, signal, WritableSignal } from '@angular/core';
+import {
+  Component,
+  OnDestroy,
+  OnInit,
+  signal,
+  WritableSignal,
+} from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { QuizService } from '../shared/services/quiz.service';
 import { CategoriesService } from '../shared/services/categories.service';
 import { Category } from 'src/_interfaces/Category';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-quiz',
   templateUrl: './quiz.component.html',
   styleUrls: ['./quiz.component.scss'],
 })
-export class QuizComponent implements OnInit {
+export class QuizComponent implements OnInit, OnDestroy {
   isQuizFinished = this.quizService.isQuizFinished;
   playerName = localStorage.getItem('playerName');
-  categoryId = 1;
-  quizCategory: WritableSignal<Category> = signal({id: 0, categoryLabel: ''});
+  categoryId = 0;
+  quizCategory: WritableSignal<Category> = signal({ id: 0, categoryLabel: '' });
+
+  routeSubscription = new Subscription();
+  categoriesSubscription = new Subscription();
 
   constructor(
     private quizService: QuizService,
@@ -23,13 +33,20 @@ export class QuizComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.route.params.subscribe((params) => {
+    this.routeSubscription = this.route.params.subscribe((params) => {
       this.categoryId = params['categoryId'];
-    });
 
-    this.categoriesService.getCategoryById(this.categoryId).subscribe((res) => {
-      this.quizCategory.set(res[0]);
+      this.categoriesSubscription = this.categoriesService
+        .getCategoryById(this.categoryId)
+        .subscribe((res) => {
+          this.quizCategory.set(res[0]);
+        });
     });
+  }
+
+  ngOnDestroy(): void {
+    this.routeSubscription.unsubscribe();
+    this.categoriesSubscription.unsubscribe()
   }
 
   goToResultPage() {
